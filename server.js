7c43049c2df9f0651c2ac7f59ac1ea9514dcf14a
creativebud <bernardpelgrim@gmail.com>
2025-08-08 21:01:04 -0700

    fixed error

--- server.js
+++ server.js
@@ -28,40 +28,15 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 });
 
 //END_ASYNC
+
+//START_SYNC
+
 var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
 console.log(hash);
 
 var result = bcrypt.compareSync(myPlaintextPassword, hash);
 console.log(result);
-//START_SYNC
-
-
-
 //END_SYNC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
