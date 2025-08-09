09c86808160f816f6a548fb97f42348f825416f8
creativebud <bernardpelgrim@gmail.com>
2025-08-08 20:42:43 -0700

    Added salt

--- server.js
+++ server.js
@@ -11,11 +11,24 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
+
+
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
+const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res);
+     });
+});
 
 //END_ASYNC
+
 
 //START_SYNC
 
@@ -53,5 +66,5 @@ const someOtherPlaintextPassword = 'pass123';
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("Listening on port:", PORT)
+  console.log("Listening on port:", PORT);
 });
